45a4f1d24e0d4cab816c395867a3f64f53516ab2
justinchong92 <hello@justinchong.io>
2025-07-08 19:39:55 +1000

    Add error handling in SignupForm and update signup function to throw errors on failure

--- src/helpers/client.ts
+++ src/helpers/client.ts
@@ -2,6 +2,9 @@ import { createClient } from "@supabase/supabase-js";
 
 import config from "@/config";
 
+console.log("Supabase URL:", config.BAAS_API_URL);
+console.log("Supabase Key:", config.BAAS_API_KEY);
+
 const client = createClient(config.BAAS_API_URL, config.BAAS_API_KEY);
 
 export default client;
